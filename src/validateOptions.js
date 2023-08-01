e83e398fb9a52d0f7ab9e38b887ccddd6208d3d8
ntsmith7 <nick@polarity.io>
2023-07-31 21:52:41 -0700

    feat: updte validateOptions

--- src/validateOptions.js
+++ src/validateOptions.js
@@ -4,30 +4,30 @@ const reduce = require('lodash/fp/reduce').convert({ cap: false });
 const validateOptions = (options, callback) => {
   const stringOptionsErrorMessages = {
     investigateUrl: 'You must provide a valid Investigation URL',
+    investigateApiKey:
+      'You must provide a valid API Key from your Cisco Umbrella Account',
     apiKey: 'You must provide a valid API Key from your Cisco Umbrella Account',
+    secretKey: 'You must provide a valid Secret Key from your Cisco Umbrella Account',
     ...((options.allowBlocklistSubmission.value ||
       options.allowAllowlistSubmission.value) && {
-      managementUrl: 'You must provide a valid Management URL',
-      networkDevicesApiKey:
-        'You must provide a valid API Key that is created using the "Umbrella Network Device" option selected on your Cisco Umbrella Account',
-      networkDevicesSecretKey:
-        'You must provide a valid Secret Key that is created using the "Umbrella Network Device" option selected on your Cisco Umbrella Account',
-      managementApiKey:
-        'You must provide a valid API Key that is created using the "Umbrella Management" option selected on your Cisco Umbrella Account',
-      managementSecretKey:
-        'You must provide a valid Secret Key that is created using the "Umbrella Management" option selected on your Cisco Umbrella Account'
+      umbrellaUrl: 'You must provide a valid Umbrella URL'
     })
   };
 
-   const stringValidationErrors = _validateStringOptions(
+  const stringValidationErrors = _validateStringOptions(
     stringOptionsErrorMessages,
     options
   );
 
   const investigateUrlValidationError = _validateUrlOption(options, 'investigateUrl');
-  const managementUrlValidationError = _validateUrlOption(options, 'managementUrl');
+  const managementUrlValidationError = _validateUrlOption(options, 'umbrellaUrl');
 
-  callback(null, stringValidationErrors.concat(investigateUrlValidationError).concat(managementUrlValidationError));
+  callback(
+    null,
+    stringValidationErrors
+      .concat(investigateUrlValidationError)
+      .concat(managementUrlValidationError)
+  );
 };
 
 const _validateStringOptions = (stringOptionsErrorMessages, options, otherErrors = []) =>
@@ -45,10 +45,12 @@ const _validateStringOptions = (stringOptionsErrorMessages, options, otherErrors
 
 const _validateUrlOption = (option, key) =>
   option[key].value && option[key].value.endsWith('//')
-    ? [{
-        key,
-        message: 'Your Url must not end with a //'
-      }]
+    ? [
+        {
+          key,
+          message: 'Your Url must not end with a //'
+        }
+      ]
     : [];
 
 module.exports = validateOptions;
